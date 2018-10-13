8308b4b46ee4d8192062ba35823f850c0e044922
Sabrine Piovesana <sabry.piovesana@gmail.com>
2018-10-13 11:19:07 -0300

    Alterações

--- Exemplo/Exemplo/Models/BancoContexto.cs
+++ Exemplo/Exemplo/Models/BancoContexto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace Example.Models
 {
@@ -18,7 +14,7 @@ namespace Example.Models
         public BancoContexto() : base("name=BancoContexto")
         {
         }
-
+        
         public DbSet<Livro> Livros { get; set; }
     }
 }
